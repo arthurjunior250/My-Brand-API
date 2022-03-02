d32000957e0575b737cc4bb4adb2e1dda068cf7d
arthur <you@example.com>
2022-03-02 16:39:33 +0200

    inquiries

---
+++ src/database/model/blog.model.js
@@ -0,0 +1,23 @@
+// import mongoose from "mongoose";
+
+// const BlogSchema = new mongoose.Schema({
+//     image: {
+//         type: String,
+//         required: true
+//     },
+//     title: {
+//         type: String,
+//         required: true
+//     },
+//     content: {
+//         type: String,
+//         required: true
+//     },
+//     createdAt: {
+//         type: Date,
+//         default: new Date()
+//     }
+// });
+
+// const Blog = mongoose.model('Blog', BlogSchema);
+// export default Blog;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./database";
+import express from 'express';
+import inquiryRoutes from './routes/inquiry.route';
+// import blogRoutes from './routes/blog.route';
+
+const server = express();
+
+// default route
+server.get('/', (req, res) => {
+    res.status(200).json({ success: true, message: "You successfully landed on our My brand app API" })
+});
+
+server.use(express.json());
+
+server.use('/api/v1/inquiries', inquiryRoutes);
+// server.use('/api/v1/blogs', blogRoutes);
+
+const port = 3000;
+
+server.listen(port, () => { console.log("Server listening on port " + port) });
