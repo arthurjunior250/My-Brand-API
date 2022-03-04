8b68bc684369874a539d9fb6a7370dba805d4840
arthur <you@example.com>
2022-03-04 11:17:31 +0200

    changes

--- src/helpers/hash-password.js
+++
@@ -1,9 +0,0 @@
-import argon from 'argon2';
-
-export const hash = async (password) => {
-    return await argon.hash(password);
-}
-
-export const verify = async (hashedPassword, plainPassword) => {
-    return await argon.verify(hashedPassword, plainPassword);
-}
