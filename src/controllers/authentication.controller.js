78865b3634c6bd89702e901540ac2326912409b1
arthur <you@example.com>
2022-03-04 10:19:18 +0200

    chore(signup):added validation

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -3,17 +3,29 @@ import { hash, verify } from '../helpers/hash-password';
 import { decodeToken, signToken } from '../helpers/jwt';
 import emailVlidator from 'email-validator';
 
-export const signup = async(req, res) => {
 
+export const signup = async(req, res) => {
     if (emailVlidator.validate(req.body.email)) {
-        let user = req.body;
+        let user = await User.findOne({
+            email: req.body.email,
+        });
+        if (user) {
+            return res.status(400).json({
+                success: false,
+                message: "Email Exists",
+            });
+        }
+        user = req.body;
         user.password = await hash(user.password);
         const newUser = await new User(user);
         newUser.save();
-        res.status(201).json({ success: true, message: 'User created', data: newUser });
+        res
+            .status(201)
+            .json({ success: true, message: "User created", data: newUser });
     } else {
         res.status(401).json({ success: false, message: "invalid email" });
     }
+
 }
 
 export const login = async(req, res) => {
