1855e6f84491be9e51b0e646c165e32973032f8f
arthur <you@example.com>
2022-03-07 11:16:49 +0200

    chore(validation): blog and query validation blog and query protection

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -1,37 +1,48 @@
 import Blog from '../database/model/blog.model';
+import { blogValidation } from "../validate/index";
+
 
 export const saveBlog = async(req, res) => {
+    const { error } = blogValidation(req.body);
+    if (error) return res.status(400).json({ message: error.details[0].message });
+    let oldblog = await Blog.findOne(req.body);
+    if (oldblog) {
+        return res.status(400).json({
+            status: "fail",
+            message: "blog Exists",
+        });
+    }
     const blog = req.body;
     const newBlog = new Blog(blog);
     await newBlog.save();
-    res.status(201).json({ success: true, data: newBlog });
+    res.status(201).json({ status: "success", data: newBlog });
 }
 
 export const getAllBlogs = async(req, res) => {
     const blogs = await Blog.find();
-    res.status(200).json({ success: true, data: blogs })
+    res.status(200).json({ status: true, data: blogs })
 }
 
 export const getById = async(req, res) => {
     const { id } = req.params;
     const blog = await Blog.findById(id);
-    if (!blog) return res.status(404).json({ success: false, message: "Blog not found" });
-    res.status(200).json({ success: true, data: blog });
+    if (!blog) return res.status(404).json({ status: "fail", message: "Blog not found" });
+    res.status(200).json({ status: true, data: blog });
 }
 
 export const updateBlog = async(req, res) => {
     const { id } = req.params;
     const updates = req.body;
     const blog = await Blog.findById(id);
-    if (!blog) return res.status(404).json({ success: false, message: "Blog not found" });
+    if (!blog) return res.status(404).json({ status: "fail", message: "Blog not found" });
     await Blog.findByIdAndUpdate(id, updates);
-    res.status(200).json({ success: true, message: "Blog updated successfully" })
+    res.status(200).json({ status: "success", message: "Blog updated successfully" })
 }
 
 export const deleteBlogById = async(req, res) => {
     const { id } = req.params;
     const blog = await Blog.findById(id);
-    if (!blog) return res.status(404).json({ success: false, message: "Blog not found" });
+    if (!blog) return res.status(404).json({ status: "fail", message: "Blog not found" });
     await Blog.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: "Task deleted", data: blog });
+    res.status(200).json({ status: "success", message: "Blog deleted", data: "Null" });
 }
--- src/database/model/blog.model.js
+++ src/database/model/blog.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema({
-    url: {
+    image: {
         type: String,
         required: true
     },
