348ff3b54d3ebe01dc7ee012cd15336a206b6364
arthur <you@example.com>
2022-03-11 15:39:46 +0200

    feature(test endpoints):query tested

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-import "./database";
-import express from 'express';
-import inquiryRoutes from './routes/inquiry.route';
-import blogRoutes from './routes/blog.route';
-import authRoutes from './routes/authentication.route';
-import commentRoutes from './routes/comment.route';
-import newsletterRoutes from './routes/newsletter.route';
-
-const server = express();
-
-// default route
-server.get('/', (req, res) => {
-    res.status(200).json({ success: true, message: "You successfully landed on My brand app API" })
-});
-
-server.use(express.json());
-
-server.use('/api/v1/inquiry', inquiryRoutes);
-server.use('/api/v1/blog', blogRoutes);
-server.use('/api/v1/authentication', authRoutes);
-server.use('/api/v1/newsletter', newsletterRoutes);
-server.use('/api/v1/comment', commentRoutes);
-
+import 'dotenv/config';
+import app from './app';
 
 const port = process.env.PORT;
 
-server.listen(port, () => { console.log("Server listening on port " + port) });
+app.listen(port, () => { console.log("Server listening on port " + port) });
